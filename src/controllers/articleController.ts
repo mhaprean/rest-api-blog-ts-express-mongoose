51d95e22ff486646580d17a2f0993697b7cb4cb7
mhaprean <marianhaprean@gmail.com>
2022-12-14 13:21:38 +0200

    update article creation to include category and tags

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import Article, { IArticle } from '../models/Article';
+import Tag from '../models/Tag';
+import Category from '../models/Category';
 import Joi from 'joi';
 
 export const getArticles = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const articles = await Article.find();
+    const articles = await Article.find().populate('user', 'name subscribers');
 
     return res.status(200).json(articles);
   } catch (error) {
@@ -40,6 +42,8 @@ export const createArticle = async (
       title: Joi.string().min(3).max(60).required(),
       description: Joi.string().min(3).required(),
       image: Joi.string(),
+      tags: Joi.array().items(Joi.string().hex().length(24)),
+      category: Joi.string().hex().length(24),
     });
 
     const { error } = joiSchema.validate(req.body);
@@ -48,12 +52,53 @@ export const createArticle = async (
       return res.status(400).send(error);
     }
 
-    const { title, description } = req.body;
-    const newArticle = { title, description, user: userId };
+    const { title, description, tags = [], category = null } = req.body;
+    const newArticle = { title, description, user: userId, tags, category };
     const article = await Article.create(newArticle);
+
+    const updateTags = await Tag.updateMany(
+      { _id: article.tags },
+      { $push: { articles: article._id } }
+    );
+
+    const updateCategory = await Category.findByIdAndUpdate(article.category, {
+      $push: { articles: article._id },
+    });
 
     return res.status(201).json(article);
   } catch (error) {
     return res.status(400).json(error);
   }
 };
+
+export const deleteArticle = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const id = req.params.id;
+
+  try {
+    const article = await Article.findById(id);
+
+    if (article) {
+      const deleteFromTags = await Tag.updateMany(
+        { _id: article.tags },
+        { $pull: { articles: article._id } }
+      );
+
+      const deleteFromCategory = await Category.findOneAndUpdate(
+        { _id: article.category },
+        { $pull: { articles: article._id } }
+      );
+    }
+
+    await article?.remove();
+
+    return res.status(200).json(article);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+// TODO - create update logic for tags and category
