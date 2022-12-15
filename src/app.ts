1aab8d4c579b8c0b1982b16d0ba0f5d3f7823c90
mhaprean <marianhaprean@gmail.com>
2022-12-15 13:45:17 +0200

    create uploads folder if doesn exists

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import fileUpload, { UploadedFile } from 'express-fileupload';
 import categoryRoutes from './routes/categoryRoutes';
 import tagRoutes from './routes/tagRoutes';
 import path from 'path';
+import fs from 'fs';
 
 const app = express();
 dotenv.config();
@@ -49,6 +50,17 @@ app.post('/api/upload', function async(req, res) {
     return res.status(400).send('No files were uploaded.');
   } // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.image as UploadedFile;
+
+
+  const dir = __dirname + '/uploads';
+
+  // if the uploads folder does not exists, we need to create it
+  if (!fs.existsSync(dir)) {
+    fs.mkdir(dir, 0o777, (err) => {
+      console.log('could not create the folder: ', err);
+      res.status(500).send({'msg': 'could not create the folder', err});
+    });
+  }
 
   const uploadPath = __dirname + '/uploads/' + sampleFile.name; // Use the mv() method to place the file somewhere on your server
   sampleFile.mv(uploadPath, function (err) {
